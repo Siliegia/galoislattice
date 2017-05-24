17bc526451ec551ce0f7767189208d900e7ada25
Maria <ga67dus@mytum.de>
2017-05-24 16:07:49 +0200

    added data

---
+++ R/southernwomenG.R
@@ -0,0 +1,20 @@
+#' Southern Women Data Set
+#'
+#' Data was collected in 1930 for ethnographic studies by Davis, Gardner and Gardner. It consists of 18 women attending 14 
+#' events as a graph object.
+#'
+#' @docType data
+#'
+#' @usage data(southernwomenG)
+#'
+#' @format An object of class \code{igraph}
+#'
+#' @keywords datasets
+#'
+#' @references Davis, A., Gardner, B. B. and M. R. Gardner.
+#' Deep South. The University of Chicago Press,
+#' Chicago, 1941.
+#'
+#' @source \href{https://networkdata.ics.uci.edu/netdata/html/davis.html}{networkdata}
+#'
+"southernwomenG"
