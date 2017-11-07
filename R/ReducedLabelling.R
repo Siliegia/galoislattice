8318cd2b20664cc5045fc04c29b76626502cd319
Maria <ga67dus@mytum.de>
2017-11-07 16:49:14 +0100

    another labeling issue resolved

--- R/ReducedLabelling.R
+++ R/ReducedLabelling.R
@@ -23,7 +23,7 @@
 #' 1,1,0,1,0,1),nrow=6)
 #' colnames(M) <- c("A", "B", "C", "D")
 #' rownames(M) <- as.character(1:6)
-#' Galois <- do_galois_lattice(M, label = "partly)
+#' Galois <- do_galois_lattice(M, label = "partly")
 #' Galois <- do_full_label(Galois, M)
 #' Galois <- do_reduced_label(Galois, M)
 #' 
@@ -32,21 +32,58 @@
 
 
 do_reduced_label <- function(GaloisGraph, OriginalGraph){ # actual function to reduce the label
-  if (!is.igraph(OriginalGraph)){ OriginalGraph <- graph.incidence(OriginalGraph)}
-  L <- V(GaloisGraph)$name
+  if(length(V(GaloisGraph)$l.name) == 0){
+    L <- V(GaloisGraph)$name
+    if (!is.igraph(OriginalGraph)){
+      OriginalGraph <- graph.incidence(OriginalGraph)
+    }}else{
+      if (!is.igraph(OriginalGraph)){ 
+        OriginalGraph <- graph.incidence(OriginalGraph)
+        V(OriginalGraph)$name <- unlist(GaloisGraph$match.name[match(V(OriginalGraph)$name, table = GaloisGraph$match.name[,2]),1])
+        L <- V(GaloisGraph)$l.name
+      }else{
+        V(OriginalGraph)$name <- unlist(GaloisGraph$match.name[match(V(OriginalGraph)$name, table = GaloisGraph$match.name[,2]),1])
+        L <- V(GaloisGraph)$l.name
+      }}
+  
   L <- lapply(L,strsplit,", ")
   L <- lapply(L, unlist)
   Newlabel1 <- lapply(L,ReduceGrlabel,OriginalGraph,"a")
   Newlabel2 <- lapply(L,ReduceGrlabel,OriginalGraph,"b")
+  if(length(V(GaloisGraph)$l.name) != 0){
+  Newname1 <- lapply(Newlabel1, function(x){GaloisGraph$match.name[match(x,GaloisGraph$match.name[,1]),2]})
+  Newname2 <- lapply(Newlabel2, function(x){GaloisGraph$match.name[match(x,GaloisGraph$match.name[,1]),2]})
   out5 <- vector("list", length(L))
+  out <- vector("list", length(L))
   for (i in 1:length(Newlabel1)){
     out5[[i]] <- toString(c(Newlabel1[[i]],Newlabel2[[i]]))
+    out[[i]] <- toString(c(Newname1[[i]],Newname2[[i]]))
   }
   if (out5[[1]] == ""){
-  out5[[1]] <- "0"}
+    out5[[1]] <- "0"}
   if (out5[[length(out5)]] == ""){
-  out5[[length(out5)]] <- "00"}
-  V(GaloisGraph)$name <- out5
+    out5[[length(out5)]] <- "00"}
+  
+  if (out[[1]] == ""){
+    out[[1]] <- "0"}
+  if (out[[length(out)]] == ""){
+    out[[length(out)]] <- "00"}
+  
+  V(GaloisGraph)$name <- out
+  V(GaloisGraph)$l.name <- out5
+  }else{
+    out5 <- vector("list", length(L))
+    for (i in 1:length(Newlabel1)){
+      out5[[i]] <- toString(c(Newlabel1[[i]],Newlabel2[[i]]))
+    }
+    if (out5[[1]] == ""){
+      out5[[1]] <- "0"}
+    if (out5[[length(out5)]] == ""){
+      out5[[length(out5)]] <- "00"}
+  
+    V(GaloisGraph)$name <- out5
+    }
+  
   return(GaloisGraph)
 }
 
